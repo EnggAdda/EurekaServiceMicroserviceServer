30ed55c06490993598a26d4df07a9e2f25ed4110
Ravi kumar gupta <111415169+EnggAdda@users.noreply.github.com>
2022-12-17 20:12:52 +0530

    EurekaService

---
+++ src/test/java/com/example/eurekaservicemicroserviceserver/EurekaServiceMicroserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaservicemicroserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceMicroserviceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
